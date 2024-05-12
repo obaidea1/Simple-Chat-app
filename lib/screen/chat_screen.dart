9efd4d687954b8597e0f9aca32187a514a4547a3
obaida <obaidazoqili@gmail.com>
2024-05-12 19:21:20 +0300

    12/5

--- lib/screen/chat_screen.dart
+++ lib/screen/chat_screen.dart
@@ -25,6 +25,7 @@ class _ChatScreenState extends State<ChatScreen> {
   void initState() {
     super.initState();
     getAvatarImage();
+    setUpPushNotification();
   }
 
   Future<void> getAvatarImage() async {
